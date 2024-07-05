34476ea8bac660ec6f5c599bc5fc4032b2610feb
freejog <167161823+freejog@users.noreply.github.com>
2024-07-05 18:36:16 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,11 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.includes(:user).order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
