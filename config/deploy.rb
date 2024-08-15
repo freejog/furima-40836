a4ac1e8e75cc9a2fbd16f550697b795514d4bd6c
freejog <167161823+freejog@users.noreply.github.com>
2024-08-15 10:14:15 +0900

    タイプミスの修正（自動デプロイ関連）

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,31 @@
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.19.1'
 
+# Capistranoのログの表示に利用する
 set :application, 'furima-40836'
 
-set :repo_url, 'git@github.com:freejog/furima-40836.git'
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:freejog/furima-40836.git'
 set :branch, 'main'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vender/bundle', 'public/system', 'public/uploads')
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.0'
 
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/my-key-pair.pem']
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/my-key-pair.pem'] 
 
+# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
+# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
