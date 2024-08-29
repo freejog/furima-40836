42c8c73d91ce63a25de3a933fc375e01e49ae23a
freejog <167161823+freejog@users.noreply.github.com>
2024-08-29 23:42:07 +0900

    commentsコントローラーの編集
    broadcastの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     return unless @comment.save
 
-    redirect_to item_path(params[:item_id])
+    ActionCable.server.broadcast 'comment_channel', { comment: @comment, user: @comment.user }
   end
 
   private
